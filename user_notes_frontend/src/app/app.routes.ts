a721dcb9f6178338fe7f1e43f29df9a5cdcebaee
kishanrajdev <kishanrajdev047@gmail.com>
2026-04-24 13:25:20 -0500

    changing route

--- user_notes_frontend/src/app/app.routes.ts
+++ user_notes_frontend/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { UserListComponent } from './components/user-list/user-list.component';
 import { UserFormComponent } from './components/user-form/user-form.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/users', pathMatch: 'full' },
+  { path: '', component: UserListComponent },
   { path: 'users', component: UserListComponent },
   { path: 'users/create', component: UserFormComponent },
   { path: 'users/:id/edit', component: UserFormComponent }
